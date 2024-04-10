3c63e3ed7eb697f67bc673dcf850a8e79e3a0697
ksahadeo <kayla.sahadeo@ufl.edu>
2024-04-10 17:29:47 -0400

    I added main() function to implement encode and decode

--- kas_Lab9.py
+++ kas_Lab9.py
@@ -5,11 +5,26 @@ def encode():
     message = []
     for number in password:
         if 48 <= ord(number) <= 57:
-            encoded.append((int(number)+3))
+            encoded.append((int(number) + 3))
     for number in encoded:
         message.append(str(number))
     message = ''.join(message)
     print("Your password has been encoded and stored!")
     return message
 
+
+def decode():
+    pass
+
+
 if __name__ == "__main__":
+    use = True
+    while use is True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        opt = input("Please enter an option: ")
+        if opt == 1:
+            encode()
+        elif opt == 2:
+            decode()
+        elif opt == 3:
+            use = False
